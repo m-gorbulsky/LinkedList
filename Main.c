89d36d2730be20b3466bc90d20bfaa329f95a72d
Mikhail Gorbulsky <mikhail.gorbulsky@live.com>
2015-11-18 13:30:23 +0200

    list_add added list_create fixed

--- Main.c
+++ Main.c
@@ -11,20 +11,54 @@ typedef struct Node
 
 typedef struct LinkedList
 {
-	struct Node head;
+	struct Node * head;
 };
 
 struct LinkedList * list_new()
 {
-	struct LinkedList *list;
+	struct LinkedList * list;
 	list = malloc(sizeof(struct LinkedList));
+	list->head = NULL;
 	return list;
 }
 
+int list_add(struct LinkedList * list, int value)
+{
+	if (list == NULL)
+	{
+		return 1;
+	}
+
+	struct Node * node = malloc(sizeof(struct Node));
+	node->value = value;
+	node->next = NULL;
+	node->prev = NULL;
+
+	if (list->head == NULL)
+	{
+		// empty list passed. Just init head.
+		list->head = node;
+	}
+	else
+	{
+		// set the previouse node for the new node
+		node->prev = list->head;
+		// set the next to the current head
+		list->head->next = node;
+		// set head to the new node
+		list->head = node;
+	}
+
+}
+
 int main() {
-		
+	
+	// test list creation
 	struct LinledList * list = list_new();
 	
+	// test list_add
+	list_add(list, 10);
+
 	return 0;
 
 
